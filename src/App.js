d76fdf1a737c9156b1c670ebd60ac109a437e457
dansmog <juwonanthony@gmail.com>
2017-06-26 23:01:45 +0100

    remove bootstrap css, use flexbox styling instead and complete chatlist component

--- src/App.js
+++ src/App.js
@@ -1,40 +1,72 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="wrapper">
-          <div className="container-fluid">
-            <div className="row">
-              <div className="col-xs-12 col-sm-3 col-md-3 chatlist no-padding">
-                <div className="header" style={{"padding": "10px 15px", "backgroundColor": "#eee"}}>
-                  <div className="row">
-                    <div className="col-xs-12 col-sm-2 col-md-2 pull-left">
-                      <img src={require('./images/profile.jpg')} style={{"width": "40px","height": "40px", "borderRadius": "50%", "overflow": "hidden", "verticalAlign": "baseline"}}/>
-                    </div>
-                    <div className="col-xs-12 col-sm-3 col-md-3 pull-right">
-                        <div className="row" style={{"marginTop": "7px"}}>
-                          <div className="col-xs-12 col-sm-6 col-md-6">
-                              <i className="icons icon-speech" style={{"fontSize": "20px", "fontWeight": "bold"}}></i>
-                          </div>
-                          <div className="col-xs-12 col-sm-6 col-md-6">
-                              <i className="icons icon-options-vertical" style={{"fontSize": "20px", "fontWeight": "bold"}}></i>
-                          </div>
-                        </div>
-                      
-                      
-                    </div>
-                  </div>
-                </div>
+      <div className="app">
+      
+          <div className="chatlist">
+            
+            <div className="chatlist--header">
+              <div className="chatlist--header__image">
+                <img src="http://www.joshfinnie.com/assets/images/josh-tm.jpeg" />
               </div>
-
-              <div className="col-xs-12 col-sm-9 col-md-9" style={{"backgroundColor": "rgb(248, 249, 251)", "minHeight": "700px", "height": "100vh"}}>
-                  <img src={require('./images/whatsapp.png')} className="img-responsive" style={{"margin": "100px auto"}}/>                    
+              <div className="chatlist--header__icons">
+                <i className="material-icons">&#xE85A;</i>
+                <i className="material-icons">&#xE5D4;</i>
               </div>
             </div>
+            <div className="chatlist--search" style={{"position": "relative"}}>
+              <input type="" placeholder="search or start a new chat"/>
+              <img src={require('./images/searchicon.png')} alt={'user profile picture'} style={{"width": "15px", "position":"absolute","bottom":"18px","left":"20px","fontSize":"15px"}} />
+  
+            </div>
+            
+            <div className="list">
+              
+              <div className="list--item">
+                <div className="list--item__image">
+                  <img src="http://www.joshfinnie.com/assets/images/josh-tm.jpeg" alt={'user profile picture'}/>
+                </div>
+                <div className="list--item__details">
+                  <h3>Fellowship</h3>
+                  <p>Bro smart: what i think this should be...</p>
+                </div>
+              </div>
+              
+              <div className="list--item">
+                <div className="list--item__image">
+                  <img src="http://www.joshfinnie.com/assets/images/josh-tm.jpeg" alt={'user profile picture'}/>
+                </div>
+                <div className="list--item__details">
+                  <h3>Fellowship</h3>
+                  <p>Bro smart: what i think this should be...</p>
+                </div>
+              </div>
+              <div className="list--item">
+                <div className="list--item__image">
+                  <img src="http://www.joshfinnie.com/assets/images/josh-tm.jpeg" alt={'user profile picture'}/>
+                </div>
+                <div className="list--item__details">
+                  <h3>Fellowship</h3>
+                  <p>Bro smart: what i think this should be...</p>
+                </div>
+              </div>
+              
+            </div>
+            
           </div>
+          
+          <div className="view">
+            <div className="view--whatsapp">
+                <img src={require('./images/whatsapp.png')} alt={"whatsapp key point"} />
+                <h2>keep you phone connected</h2>
+                <p>whatsapp connects to your phone to sync messages. To reduce data usage, connect your phone to wifi</p>
+            </div>
+            <div className="view--chats"></div>
+          </div>
+    
       </div>
     );
   }
